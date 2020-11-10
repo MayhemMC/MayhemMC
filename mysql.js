605dc66e245a4026f784862c8d78f3674934eb29
Josh Merlino <joshua.merlino@gmail.com>
2020-11-09 22:36:17 -0500

    change credentials cus im an idiot

--- mysql.js
+++ mysql.js
@@ -2,7 +2,7 @@ const db = require("mysql-promise")();
 
 module.exports = async function() {
 
-	const params = YAML.parse(await fs.readFile(path.join(__dirname, "mysql.yml"), "utf8")).mysql;
+	const params = YAML.parse(await fs.readFile(path.join(__dirname, "config.yml"), "utf8")).mysql;
 	db.configure(params, require("mysql2"));
 	return db;
 
