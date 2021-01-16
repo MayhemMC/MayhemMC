0cb0ab7ee99926bf076b0b7dd43f8eee1536bced
Josh Merlino <joshua.merlino@gmail.com>
2021-01-16 15:08:30 -0500

    add donations api

--- api/donations.js
+++ api/donations.js
@@ -1,21 +1,48 @@
-module.exports = async function(req, res) {
+import chalk from "chalk";
+import namemc from "namemc";
 
-	// Get params
-	const params = { ...req.body, ...req.query };
-	let { limit = 5 } = params;
+export default req => new Promise(async function(resolve, reject) {
 
-	// Convert limit to int
-	limit = parseInt(limit);
-	if(limit === -1) limit = 50000;
+	// Get limit
+	let limit = parseInt(req.query.limit || req.query.max || 5);
+	if(limit === -1) limit = Infinity;
 
-	// Get votes from database
-	const [ donations ] = await mysql.query(`SELECT * FROM donations ORDER BY timestamp DESC LIMIT ${limit}`)
+	// If limit is less than 1
+	if(limit < 1) return reject("Limit can not be less than 1.");
+	if(isNaN(limit)) return reject("Limit must be a number.");
 
-	// Respond to request
-	res.json({
-		success: true,
-		limit: limit === 50000 ? false : limit,
-		donations
-	})
+	// Get donations from database
+	let [ donations ] = await mysql.query(`SELECT * FROM donations ORDER BY timestamp DESC${isFinite(limit) ? ` LIMIT ${limit}` : ""}`);
+	donations = await Promise.allSettled(donations.map(donation => {
+		return new Promise(async function(resolve, reject) {
+			try {
 
-}
+				// Get other properties
+				const { uuid, timestamp, package: rank } = donation;
+
+				// Get current username from namemc
+				const { currentName: name } = await namemc.lookupUUID(uuid);
+
+				// Get prefix from group
+				const [ permissions ] = await mysql.query(`SELECT * FROM luckperms_group_permissions WHERE name="${rank.toLowerCase()}"`);
+				const prefix = permissions.filter(({ permission: node }) => node.includes("prefix"))[0].permission.split(".")[2];
+
+				// Return with data
+				resolve({ name, uuid, package: rank.toLowerCase(), prefix, timestamp: new Date(timestamp).getTime() })
+
+			} catch (e) {
+
+				// Reject and log error
+				console.error(chalk.red("[ERROR]"), e)
+				reject(e);
+
+			}
+		})
+	}));
+
+	donations = donations.filter(element => element.status === "fulfilled").map(element => element.value)
+
+	// Resolve API with result
+	resolve({ limit, donations });
+
+});
