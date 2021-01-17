c2f196f411c6621b146c60a7ad8cb11efa21fec8
Josh Merlino <joshua.merlino@gmail.com>
2021-01-17 13:57:28 -0500

    add back servers api

--- api/servers.js
+++ api/servers.js
@@ -1,59 +1,56 @@
-let cache = undefined;
+import path from "path";
+import { stripFormats } from "minecraft-text";
+import { promises as fs } from "fs";
+import YAML from "yaml";
 
-module.exports = async function(req, res) {
+export default req => new Promise(async function(resolve, reject) {
 
-	// Attempt to resolve server from cache
-	const cached = cache;
-	if(cached !== undefined && cached.expires > Date.now()) {
-		const cached_response = { ...cached };
-		delete cached_response.expires;
-		return res.json({ ...cached_response, cached: true });
+	// Get timeout
+	const timeout = parseInt(req.query.timeout || 15);
+
+	// If timeout is less than 1
+	if(timeout < 1) return reject("Timeout can not be less than 1.");
+	if(isNaN(timeout)) return reject("Timeout must be a number.");
+
+	// Initialize servers
+	const servers = [];
+
+	// Get all active server ports
+	const ports = {};
+	const { servers: srvs } = YAML.parse(await fs.readFile(path.resolve(MMC_ROOT, "bungee", "config.yml"), "utf8"));
+	Object.keys(srvs).map(server => ports[server] = parseInt(srvs[server].address.split(":")[1]));
+
+	// Initialize partial
+	let partial = false;
+
+	// Get server menu
+	const menu = YAML.parse(await fs.readFile(path.resolve(MMC_ROOT, "lobby/plugins/ChestCommands/menu", "servers.yml"), "utf8"));
+
+	// Query each server
+	for (const server in ports) {
+		const port = ports[server];
+		const result = await mcquery(port, timeout);
+		if(result === null) {
+			partial = true;
+		} else {
+
+			// Get server version
+			const { version } = result;
+
+			// Get properties from the server menu
+			const { NAME: name_formatted, MATERIAL: icon, LORE: description } = menu[server];
+			const name = stripFormats(name_formatted, "&");
+
+			description.shift();
+			description.shift();
+
+			// Push to response
+			servers.push({ server, version, name_formatted, name, icon, description: stripFormats(description.join("\n"), "&") });
+
+		}
 	}
 
-	// Get servers from bungee config
-	const { servers, player_limit } = YAML.parse(await fs.readFile(path.join(MMC_ROOT, "bungee/config.yml"), "utf8"));
-	const { WHITELIST_PLAYERS, PROTOCOL_VERSION, MAINTENANCE_MODE_ENABLED } = YAML.parse(await fs.readFile(path.join(MMC_ROOT, "bungee/plugins/BungeeAdvancedProxy/config.yml"), "utf8"));
-	const { VERSION, MOTD, COUNT } = PROTOCOL_VERSION;
+	// Respons to request
+	resolve({ servers, partial });
 
-	// Parse server names and descriptions from the player count
-	const names = [];
-	const desc = [];
-	COUNT.map(line => {
-		if(line.includes("&f: &7({Server@")) {
-			names.push(line.split("&f: &7({Server@")[0].replace(/\s\s+/g, ""));
-			desc.push([]);
-		}
-		if(line.includes("&f● &")) {
-			desc[desc.length - 1] = [ ...desc[desc.length - 1], line.replace(/\s\s+/g, "") ]
-		}
-	})
-
-	// Query bungee server
-	const bungeequery = await query(25577).catch(() => ({ map: null, players: [] }))
-
-	// Get all servers
-	const serverList = {};
-	for(server in servers) serverList[server] = await mmcApi("server", { server });
-
-	// Formulate final response and respond
-	const response = {
-		success: true,
-		maintenance_mode: MAINTENANCE_MODE_ENABLED,
-		supported_protocols: VERSION[0].split(/\&[0-9a-f]/gm)[1],
-		administrators: WHITELIST_PLAYERS,
-		discord_invite: "4FBnfPA",
-		max_players: player_limit,
-		software: bungeequery.map,
-		players: bungeequery.players,
-		motd: MOTD[Math.floor(Math.random() * MOTD.length)].replace(/\;/gm, "\n").replace(/section/gm, "center"),
-		servers: serverList
-	};
-
-	// Send response
-	res.json({ ...response, cached: false})
-
-	// Cache response for 5 seconds
-	response.expires = Date.now() + 5000;
-	cache = response;
-
-}
+});
