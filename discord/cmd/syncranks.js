faa07f81ee2aac8cf795e0c57c3b64a93e34c9e5
Josh Merlino <joshua.merlino@gmail.com>
2021-01-18 10:57:41 -0500

    add back syncranks command

--- discord/cmd/syncranks.js
+++ discord/cmd/syncranks.js
@@ -1,3 +1,5 @@
+import namemc from "namemc";
+
 export default async function(args, message) {
 
 	const { channel, member } = message;
@@ -14,26 +16,35 @@ export default async function(args, message) {
 	const role_ids = [ Roles.VIP, Roles.WARRIOR, Roles.HERO, Roles.LEGEND, Roles.TITAN ];
 	const role_names = [ "vip", "warrior", "hero", "legend", "titan" ];
 	const role_formatted = [ "&2[&aVIP&2]", "&3[&bWARRIOR&3]", "&5[&dHERO&5]", "&6[&eLEGEND&6]", "&4[&cTITAN&4]" ];
-	const role_codes = [ "vip", "lite", "mid", "high", "titan" ];
 
 	// Get all donators from database
 	const [ donators ] = await mysql.query(`SELECT * FROM \`donations\``);
 
-	const { servers } = YAML.parse(await fs.readFile(path.join(MMC_ROOT, "bungee/config.yml"), "utf8"));
-	donators.map(async ({ name, package: rank }) => {
+	// Iterate over donators
+	await donators.map(async ({ name, package: rank }) => {
 
+		// Get rank
 		const role = role_names.indexOf(rank.toLowerCase());
-		const { discord_id } = await mmcApi("player", { name });
-		if(discord_id !== false) {
+
+		// Get namemc details
+		const [{ currentName, uuid }] = await namemc.lookupName(name);
+
+		// Get discord snowflake from registration
+		const [[ registration ]] = await mysql.query(`SELECT * FROM discord_players WHERE uuid="${uuid}"`);
+		const discord_id = registration !== undefined ? registration.discordid : null;
+
+		console.log({ currentName, uuid, rank });
+
+		// Send commands
+		await inject("lobby", `lp user ${name} parent set ${role_names[role]}`);
+		await inject("bungee", `alert &7Thank you &f${name}&7 for supporting the server! You have recieved your ${role_formatted[role]}&7 rank!`);
+
+		if(discord_id !== null) {
 			const specimen = await (await client.guilds.fetch("708050277957238784")).members.fetch(discord_id)
 		    role_ids.map(role => specimen.roles.remove(role));
 			specimen.roles.add(role_ids[role]);
 		}
 
-		for (const server of Object.keys(servers)) {
-			await mmcExec(server, `bc Thank you &f${name}&7 for supporting the server! You have recieved your ${role_formatted[role]}&7 rank!`);
-			await mmcExec(server, `lp user ${name} parent set ${role_codes[role]}`);
-		};
 	})
 
 	const embed = new MessageEmbed()
