d7290d1be75c0948b66d4a50dfd82cd6f72d7df8
Josh Merlino <joshua.merlino@gmail.com>
2021-01-21 10:34:10 -0500

    add back votes page

--- src/views/Vote.js
+++ src/views/Vote.js
@@ -1,10 +1,35 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { Card } from "@photoncss/Card";
+import React, { Fragment } from "react";
 import { Col, Container, Row } from "@photoncss/Layout";
 import { Toolbar, ToolbarTitle, ToolbarSpacer } from "@photoncss/Toolbar";
 import { Icon } from "@photoncss/Icon";
 import Markdown from "components/Markdown";
 import { VoteList } from "components/PlayerList";
+import { Card, CardTitle } from "@photoncss/Card";
+import { List, ListItem } from "@photoncss/List";
+
+export function VoteLinks() {
+
+	const links = [
+		"https://minecraftservers.org/vote/583568",
+		"https://minecraft-server-list.com/server/459450/vote/",
+		"https://minecraft-mp.com/server/255961/vote/",
+		"https://topg.org/Minecraft/in-605738"
+	];
+
+	return (
+		<Card>
+			<CardTitle>Vote Links</CardTitle>
+			<hr/>
+			<List style={{ borderLeft: 0, borderRight: 0 }}>
+				{ links.map((link, key) => (
+					<a href={link} target="_blank" key={key}>
+						<ListItem leadingIcon="link">Vote Link #{key+1}</ListItem>
+					</a>
+				))}
+			</List>
+		</Card>
+	)
+}
 
 
 // Render view
@@ -24,6 +49,7 @@ function View() {
 
 					<Col sm={12} lg={4}>
 						<VoteList/>
+						<VoteLinks/>
 					</Col>
 
 					<Col sm={12} lg={8}>
