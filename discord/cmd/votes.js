1b17689a75b73f736aff51ad442a7a8d6bb1edd7
Josh Merlino <joshua.merlino@gmail.com>
2021-01-21 09:53:56 -0500

    fix votes command

--- discord/cmd/votes.js
+++ discord/cmd/votes.js
@@ -4,17 +4,15 @@ import ordinal from "ordinal";
 export default async function(args, message) {
 
 	const { channel } = message;
-	const { votes } = await api("votes", { limit: -1 });
-
-	const sum = votes.reduce((total, { amount }) => amount + total, 0);
+	const { votes, numvotes: sum, numvoters } = await api("votes");
 
 	const embed = new MessageEmbed();
 	embed.setColor(Color.INFO);
 	embed.setTitle(`Votes (${dayjs().format("MMMM")})`)
 
 	const lines = [];
-	lines.push(`**${votes.length}** players voted a total **${sum}** times this month.\n`);
-	votes.slice(0, 5).map(({ name, amount }, key) => lines.push(`**${ordinal(key + 1)}** - \`${name}\` - **${amount}** votes`))
+	lines.push(`**${numvoters}** players voted **${sum}** times this month.\n`);
+	votes.map(({ name, amount }, key) => lines.push(`**${ordinal(key + 1)}** - \`${name}\` - **${amount}** votes`))
 	embed.setDescription(lines.join("\n"));
 	return await channel.send(embed);
 
