041a8daa6d887cd48c4f418fa062ead694e081b2
Josh Merlino <joshua.merlino@gmail.com>
2021-01-21 10:33:29 -0500

    remove server list from sidenav

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -12,23 +12,16 @@ export default function Component() {
 	const [ i, forceUpdate ] = useReducer(x => x + 1, 0);
 	i === 0 && Photon.hooks.push(forceUpdate);
 
-	const [{ servers }, _servers ] = useState({ servers: {} });
-	useEffect(() => Object.keys(servers).length === 0 && app.api("servers").then(_servers));
-
 	return (
 		<Fragment>
 			<Drawer id="web-nav">
 				<img src={ app.static("icon-transparent.png") } alt="" style={{ margin: "0 auto", width: "66%" }}/>
 				<hr/>
 				<Route to="/" icon="home">Home</Route>
-				<Route to="/performance" icon="speed">Server Performance</Route>
+
 				<Route to="/store" icon="shopping_cart">Store</Route>
 				<Route to="/vote" icon="how_to_vote">Vote</Route>
-				<Fragment>
-					<hr/>
-					<Subheader>Servers</Subheader>
-					{Object.keys(servers).filter(server => servers[server].description !== "").map(server => <Route to={`/server/${server}`} key={Object.keys(servers).indexOf(server)}>{stripFormats(servers[server].display_name, "&")}</Route>)}
-				</Fragment>
+
 			</Drawer>
 		</Fragment>
 	)
