5764dbe0e5e3ce52e129edc1cff2028590b34b12
Josh Merlino <joshua.merlino@gmail.com>
2021-01-28 17:47:19 -0500

    update formatting

--- api/discord.js
+++ api/discord.js
@@ -1,8 +1,4 @@
-import path from "path";
-import { promises as fs } from "fs";
-import YAML from "yaml";
-
-export default req => new Promise(async function(resolve, reject) {
+export default () => new Promise(async function(resolve) {
 
 	resolve({ ...(await discordStat()) })
 
