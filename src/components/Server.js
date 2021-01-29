6c54649abe2a87ffc2bde2aafa2523e78c83ade9
Josh Merlino <joshua.merlino@gmail.com>
2021-01-29 14:41:57 -0500

    add dynmap tab

---
+++ src/components/Server.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { Col, Row, Container } from "@photoncss/Layout";
+import { ServerList } from "components/PlayerList";
+import { Button } from "@photoncss/Button";
+
+export function Overview(server) {
+	return (
+		<Container>
+			<Row>
+
+				<Col sm={12} lg={4}>
+					<ServerList spec={server.name}/>
+				</Col>
+
+			</Row>
+		</Container>
+	);
+}
+
+export function Plugins(server) {
+	return (
+		<Container>
+			<Row>
+
+				plugins
+
+			</Row>
+		</Container>
+	);
+}
+
+export function Dynmap(server) {
+
+	const uuid = Photon.guid();
+
+	useEffect(function() {
+		$(window).on("resize", () => $(`#${uuid}`).css({ height: window.innerHeight - $("footer").height() - 128 }));
+		requestAnimationFrame(() => $(`#${uuid}`).css({ height: window.innerHeight - $("footer").height() - 128 }))
+		return () => $(window).off("resize");
+	})
+
+	return (
+		<div style={{ position: "relative" }}>
+			<iframe src={`/dynmap/${server.name}/#`} frameBorder="0" className="full-frame" id={uuid}></iframe>
+			<a href={`//${location.hostname}${location.port === "" ? "" : ":" + location.port}/dynmap/${server.name}/`} target="_blank">
+				<Button variant="raised" style={{ position: "absolute", right: 8, bottom: 8 }}>fullscreen</Button>
+			</a>
+		</div>
+	);
+}
--- src/views/Server.js
+++ src/views/Server.js
@@ -1,14 +1,11 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
-import { Col, Container, Row } from "@photoncss/Layout";
 import { Toolbar, ToolbarTitle, ToolbarSpacer } from "@photoncss/Toolbar";
 import { Icon } from "@photoncss/Icon";
-import Markdown from "components/Markdown";
-import { VoteList } from "components/PlayerList";
-import { Card, CardTitle } from "@photoncss/Card";
-import { List, ListItem } from "@photoncss/List";
-import { ServerList } from "components/PlayerList";
+import { Tab, Tabs, TabContent } from "@photoncss/Tabs";
 import MCText from "components/MCText";
+
+import { Overview, Dynmap, Plugins } from "components/Server";
 
 // Render view
 function View() {
@@ -32,12 +29,34 @@ function View() {
 
 	// Fetch state
 	useEffect(function() {
-		const server = location.pathname.split("/server/")[1];
-		if(state.server.name !== server.toLowerCase()) app.api("server", { server }).then(setState);
+		const server = location.pathname.split("/server/")[1].split("/")[0];
+		const tab = location.pathname.split(`/server/${server}`)[1].split("/")[1] || "overview";
+		if(state.server.name !== server.toLowerCase()) app.api("server", { server }).then(newState => {
+			requestAnimationFrame(() => {
+				Photon.reload();
+				setTimeout(() => {
+					const setTab = ["overview", "plugins", "dynmap"].indexOf(tab.toLowerCase());
+					$(".tabs").children(".tab").eq(setTab).click();
+				}, 10);
+			});
+
+			setState(newState);
+		});
 	})
 
+	// If state pending
+	if(state.success === null) return null;
+
 	// If no server
-	if(state.success === false) return <Redirect to="/"/>
+	if(state.success === false) return <Redirect to="/"/>;
+
+	// Push new URL to tab
+	function push({ target }) {
+		let tab = $(target).attr("for");
+			tab = tab === "overview" ? "" : `/${tab}`;
+		const url = `/server/${state.server.name}${tab}`;
+		history.pushState(null, null, url);
+	}
 
 	// Return default structure
 	return (
@@ -46,19 +65,28 @@ function View() {
 			<Toolbar color="light-bg" variant="raised" size="contains-tabs" position="fixed">
 				<Icon onClick={() => Photon.Drawer("#web-nav").open()}>menu</Icon>
 				<ToolbarTitle><MCText delimiter="&">{state.server.name_formatted}</MCText></ToolbarTitle>
+				<Tabs>
+					<Tab htmlFor="overview" onClick={push}>Overview</Tab>
+					<Tab htmlFor="plugins" onClick={push}>Plugins ({Object.keys(state.server.plugins).length})</Tab>
+					{ state.server.plugins.hasOwnProperty("dynmap") && <Tab htmlFor="dynmap" onClick={push}>Dynmap</Tab> }
+				</Tabs>
 			</Toolbar>
 			<ToolbarSpacer/>
 
-			<Container>
-				<Row>
+				<TabContent id="overview">
+					<Overview {...state.server}/>
+				</TabContent>
 
-					{ state.success === true && <ServerList spec={state.server.name}/> }
+				<TabContent id="plugins">
+					<Plugins {...state.server}/>
+				</TabContent>
 
-				</Row>
-			</Container>
+				<TabContent id="dynmap">
+					<Dynmap {...state.server}/>
+				</TabContent>
 		</Fragment>
 	)
 }
 
 // Export View and route
-export default { View, route: "/server/:server" }
+export default { View, route: "/server/:server?/:tab" }
